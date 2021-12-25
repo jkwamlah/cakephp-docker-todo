7f621eb1a9f963ca581eed46a1890a1897e71881
jkwamlah <jkwamlah@outlook.com>
2021-12-25 15:20:24 +0000

    initial commit

---
+++ cakephp/templates/Tasks/add.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Task $task
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('List Tasks'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="tasks form content">
+            <?= $this->Form->create($task) ?>
+            <fieldset>
+                <legend><?= __('Add Task') ?></legend>
+                <?php
+                    echo $this->Form->control('title');
+                    echo $this->Form->control('description');
+                    echo $this->Form->control('scheduled_time', ['empty' => true]);
+                    echo $this->Form->control('status');
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
---
+++ cakephp/templates/Tasks/index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Task[]|\Cake\Collection\CollectionInterface $tasks
+ */
+?>
+<div class="tasks index content">
+    <?= $this->Html->link(__('New Task'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+    <h3><?= __('Tasks') ?></h3>
+    <div class="table-responsive">
+        <table>
+            <thead>
+                <tr>
+                    <th><?= $this->Paginator->sort('id') ?></th>
+                    <th><?= $this->Paginator->sort('title') ?></th>
+                    <th><?= $this->Paginator->sort('description') ?></th>
+                    <th><?= $this->Paginator->sort('scheduled_time') ?></th>
+                    <th><?= $this->Paginator->sort('status') ?></th>
+                    <th><?= $this->Paginator->sort('created') ?></th>
+                    <th class="actions"><?= __('Actions') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($tasks as $task): ?>
+                <tr>
+                    <td><?= $this->Number->format($task->id) ?></td>
+                    <td><?= h($task->title) ?></td>
+                    <td><?= h($task->description) ?></td>
+                    <td><?= h($task->scheduled_time) ?></td>
+                    <td><?= h($task->status) ?></td>
+                    <td><?= h($task->created) ?></td>
+                    <td class="actions">
+                        <?= $this->Html->link(__('View'), ['action' => 'view', $task->id]) ?>
+                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $task->id]) ?>
+                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $task->id], ['confirm' => __('Are you sure you want to delete # {0}?', $task->id)]) ?>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="paginator">
+        <ul class="pagination">
+            <?= $this->Paginator->first('<< ' . __('first')) ?>
+            <?= $this->Paginator->prev('< ' . __('previous')) ?>
+            <?= $this->Paginator->numbers() ?>
+            <?= $this->Paginator->next(__('next') . ' >') ?>
+            <?= $this->Paginator->last(__('last') . ' >>') ?>
+        </ul>
+        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+    </div>
+</div>
